eaafb98103d7cac3f79f22edcf23045898107b2c
Valente Meza <kingracoon64@gmail.com>
2017-01-20 13:37:54 -0700

    <html>, <header>, <body>, added to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="utf-8"
 </head>
-<title></title>
-<header></header>
+<title>User Example</title>
+<header><h1>Potential Employer</h1> Persona</header>
     <body>
+    <main><h2>Example Employer</h2>
+
+
+    </main>
 
     </body>
 </html>
