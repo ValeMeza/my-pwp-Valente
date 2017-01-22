6351ab1980a1008045bed3a415af7e67cb375392
Valente Meza <kingracoon64@gmail.com>
2017-01-21 18:43:50 -0700

    Use Case completed.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,22 +8,26 @@
     <body>
     <main><h2>Example Employer</h2>
         <ul>
-            <li>Name:</li>
+            <li>Name:John/Jane Smith's</li>
             <li>Age:Mid 30's</li>
-            <li>Occupation:Recruiter, HR employee</li>
-            <li>Technology:Average desktop running windows 7, 10, OSX possibly cell phone android,apple. </li>
+            <li>Occupation:Recruiters, HR employee's</li>
+            <li>Technology:Average desktop running windows 7, 10, Mac possibly cell phone android,apple. </li>
             <li>Needs:Looking to hire a full stack developer, with proper skills and motivation for the field.</li>
-            <li></li>
+            <li>Will be abel to arrive on my personal website and see examples of my work, how to reach me, get to my github to check recent work and see a about me.  </li>
         </ul>
-        <h2>Use Case</h2>
+        <h2>Use Case:</h2>
+        John Smith has just arrived to work and has been searching linkin.com  all day searching for a proficient full stack employee, after arriving at my site he is lead to a tour of my most recent projects; capstone, personal website itself, and any other repositories I have made or contributed to. Once he has explored my work, explored my github link, and have read my bio, they decide I am a right fit and click my link to contact me.
+
         <h2>Example Flow</h2>
-        <nl>
+        <ol>
             <li></li>
             <li></li>
             <li></li>
             <li></li>
             <li></li>
-        </nl>
+            <li></li>
+            <li></li>
+        </ol>
 
     </main>
 
