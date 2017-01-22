e327bb2524033d950d189267532ac86fcc774723
Valente Meza <kingracoon64@gmail.com>
2017-01-21 18:17:59 -0700

    Example Employer added; Name, Age, Occupation, Technology, Needs Use Case added Example Flow added

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,15 +8,22 @@
     <body>
     <main><h2>Example Employer</h2>
         <ul>
-            <li>Name:Joe Bigwig</li>
-            <li>Age:50</li>
-            <li>Occupation:Bussiness owner</li>
-            <li>Technology:</li>
-            <li>Needs:</li>
+            <li>Name:</li>
+            <li>Age:Mid 30's</li>
+            <li>Occupation:Recruiter, HR employee</li>
+            <li>Technology:Average desktop running windows 7, 10, OSX possibly cell phone android,apple. </li>
+            <li>Needs:Looking to hire a full stack developer, with proper skills and motivation for the field.</li>
             <li></li>
         </ul>
         <h2>Use Case</h2>
         <h2>Example Flow</h2>
+        <nl>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </nl>
 
     </main>
 
