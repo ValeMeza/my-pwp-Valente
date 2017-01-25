ca6c32ec80cfbdcebba7f0904da64d2a19d076cc
Valente Meza <kingracoon64@gmail.com>
2017-01-24 23:08:54 -0700

    Whole User Example changed.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,35 +1,40 @@
 <!DOCTYPE html>
 <html lang="eng">
-<head>
-    <meta charset="utf-8">
-</head>
-<title>User Example</title>
-<header><h1>Potential Employer</h1></header>
+    <head>
+        <meta charset="utf-8">
+        <title>User Example</title>
+    </head>
     <body>
-    <main><h2>Example Employer</h2>
-        <ul>
-            <li>Name:John/Jane Smith's</li>
-            <li>Age:Mid 30's</li>
-            <li>Occupation:Recruiters, HR employee's</li>
-            <li>Technology:Average desktop running windows 7, 10, Mac possibly cell phone android,apple. </li>
-            <li>Needs:Looking to hire a full stack developer, with proper skills and motivation for the field.</li>
-            <li>Goals for user base: Will be abel to arrive on my personal website and see examples of my work, how to reach me, get to my github to check recent work and see a about me. Reduce clutter while keeping attention. Keep in mind for mobile use and stay away from making the entire site links to other sites add work on page itself keeping clutter away from the first displayed page. Give the user natural progression from a bio, to work done, to contact info.  </li>
-        </ul>
-        <h2>Use Case:</h2>
-        John Smith has just arrived to work and has been searching linkin.com  all day searching for a proficient full stack employee, after arriving at my site he is lead to a tour of my most recent projects; capstone, personal website itself, and any other repositories I have made or contributed to. Once he has explored my work, explored my github link, and have read my bio, they decide I am a right fit and click my link to contact me.
+        <header>
+            <h1>Potential Employer</h1>
+        </header>
+        <main>
+            <h2>Example Employer</h2>
+            <ul>
+                <li>Name: June Warwick</li>
+                <li>Age: 34</li>
+                <li>Profession: June Warwick is the head of a recent Web Devolpment startup looking to take contracts from everywhere, government, local business does'nt matter! She has a team of intermediate programmers that are working on 3 to 4 projects at a time while taking care of any tickets and bugs that come there way.</li>
+                <li>Technology: Employees bring there own computers; windows 10 and Mac's with the company having their own web hosting servers. June herself uses a Mac pro Desktop and a Macbook pro laptop on the go. </li>
+                <li>Attitudes and Behaviors: June spends most of her time maintaining the company contracts, company image, and coding as well as most HR work. On her free time she still spends all her time on her laptop where she is a huge networking fiend. Twitter is her best friend,where she posts most of her local contributions and when she attends local developer workshops, linkin to help contract more work. With such a competitive nature with contracts she knows its only a matter of time till until she decides to expand.</li>
+                <li>Frustrations and Needs: June is looking to hire a junior Web Developer to do bug testing and other small task to allow the other developers to shift focus on other projects to meet deadlines for other projects. She is trying her best to diversify while getting the best possible candidate unfortunately most junior programmers have been more work then they claim to be.</li>
+            </ul>
+            <p>Goals: Junes' main goal at this point is to hire someone that does'nt need hand holding and can learn to keep up with the pace. She would prefer to diversify to hopefully bring in more intuitive ideas, but still needs someone with basic understanding of HTML, Java, PHP,CSS, and mySQL</p>
+            <h2>Use Case:</h2>
+            <p>After a long day of bug fixes. She spends the afternoon posting retweets and plans to attend a code for abq get together. While she does a bit of networking, we meet and I give her my URL to my website. After checking my website and checking my gitHub and linkid she decides to give me an opportunity to to fix bugs. After a month of learning there system im able to keep up with there normal day to day projects and move from junior to intermediate programmer.</p>
 
-        <h2>Example Flow</h2>
-        <ol>
-            <li>Employer searching linkin, arrives at my profile.</li>
-            <li>My profile contains my resume and a link to my personal website.Employer clicks my link to my site</li>
-            <li>Information is sent and received for the url</li>
-            <li>Employer reads my bio on the front page and scrolls down seeing my capstone contribution, links to other repositories, and my Github information/link.</li>
-            <li>Scrolling down the site they click the contact me link and it directs them to complete an email to contact me.</li>
-            <li>Send is clicked, a confirmation is displayed.</li>
-            <li>Email is sent and I receive it.</li>
-        </ol>
-
-    </main>
-
+            <h2>Example Flow</h2>
+            <ol>
+                <li>June gets home and types my website in.</li>
+                <li>Information is sent and received for the url</li>
+                <li>June reads my bio on the front page and scrolls down seeing my capstone contribution, links to other
+                    repositories, and my Github information/link.
+                </li>
+                <li>Scrolling down the site they click the contact me link and it directs them to complete an email to contact
+                    me.
+                </li>
+                <li>Send is clicked, a confirmation is displayed.</li>
+                <li>Email is sent and I receive it.</li>
+            </ol>
+        </main>
     </body>
 </html>
