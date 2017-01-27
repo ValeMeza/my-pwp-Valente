b6aa7a8f22290327be7a183bf5fa73495da1b052
Valente Meza <kingracoon64@gmail.com>
2017-01-26 22:50:28 -0700

    starting milestore-2a

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,7 +14,6 @@
                 <li>Audience: This site will be devolped with a professional audience in mind, to better create network opportunities within the tech industry.</li>
                 <li>Goal: My primary goal is to provied easy access for contact and to focus on more acomplished work for potential employers to view that other sites wont allow me to showcase.</li>
             </ul>
-
             <h2>Example Employer</h2>
             <ul>
                 <li>Name: June Warwick</li>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,15 @@
+<html lang="eng">
+    <head>
+        <meta charset="utf-8">
+        <title>Content Strategy</title>
+    </head>
+    <body>
+        <header>
+        Welcome!
+        </header>
+        <main>
+
+        </main>
+    </body>
+
+</html>
