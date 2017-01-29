a2f918dae17d129c82a3b2f1855087d3cefa17c6
Valente Meza <kingracoon64@gmail.com>
2017-01-28 19:15:29 -0700

    Content strategy added

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,11 +5,14 @@
     </head>
     <body>
         <header>
-        Welcome!
+        Valente's Personal website.
         </header>
         <main>
-
+            <p>Welcome, my name is Valente Meza I am an up and coming Front End Web Developer eager to share my work, invite more opportunist's, continue to learn and contribute to the tech industry!
+            About Me: As a junior developer I have concentrated my career focusing on back end coding; reading the documentation for PHP, practicing with mySQL, Object Oriented, working with Angler2 etc... I enjoy team work, take direction well while maintaining a voice on the project. I enjoy a challenge and welcome new skill sets. I am a jack of all trades, master of none, but better then a master of one.
+                Portfolio: Here is example of my latest work...
+                Contact:<a href="https://www.linkedin.com/in/valente-meza-734202133">Linkedin</a><a href="https://twitter.com/V_For_Valente">Twitter</a>
+            </p>
         </main>
     </body>
-
 </html>
