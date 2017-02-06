304e8478d0da8dda55929174f938fb3d4b11e3ff
Valente Meza <kingracoon64@gmail.com>
2017-02-05 22:43:04 -0700

    indentation fixed

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,26 +9,36 @@
     <header>
         <h1>Valente's Personal website.</h1>
     </header>
-        <main>
-            <ul>
-                <li>
-                    <h2>Content Strategy</h2> My site will be one page that will consist of three sections,<br> Welcome, short introduction, with about me followed right after.<br>My portfolio will be an interactive window that can display my Github or the result of my code. </li>
-                <li>
-                    <h2>Welcome:</h2> My name is Valente Meza. I am an up and coming Front End Web Developer eager to share my work, invite more opportunities, continue to learn and contribute to the tech industry!
-                </li>
-                <li>
-                    <h2>About Me:</h2> As a junior developer I have concentrated my career focusing on back end coding; reading the documentation for PHP, practicing with mySQL, Object Oriented, working with Angular2, etc... I enjoy team work, taking direction well, while maintaining a voice on the project. I enjoy a challenge and welcome new skill sets. I am a jack of all trades, master of none, but better then a master of one.
-                </li>
-                <li>
-                    <h2>Portfolio:</h2> This will be a gallery of the work I've done recently as well as example work to encourage others to step into web development.
-                </li>
-                <li>
-                    <h2>Contact:</h2> Section to email me.<a href="https://www.linkedin.com/in/valente-meza-734202133">Linkedin,</a>
-                    <a href="https://twitter.com/V_For_Valente">Twitter</a>
-                </li>
-            </ul>
-            <p>My wire frame was created using <a href="https://www.draw.io/" class="website">Draw.io</a></p>
-            <img src="images/wireframe.png">
-        </main>
+    <main>
+        <ul>
+            <li>
+                <h2>Content Strategy</h2> My site will be one page that will consist of three sections,<br>
+                Welcome, short introduction, with about me followed right after.<br>
+                My portfolio will be an interactive window that can display my Github or the result of my code.
+            </li>
+            <li>
+                <h2>Welcome:</h2> My name is Valente Meza.<br>
+                I am an up and coming Front End Web Developer eager to share my work,<br>
+                invite more opportunities,<br>
+                continue to learn and contribute to the tech industry!
+            </li>
+            <li>
+                <h2>About Me:</h2> As a junior developer I have concentrated my career focusing on back end coding;<br>
+                reading the documentation for PHP, practicing with mySQL, Object Oriented, working with Angular2, etc...<br>
+                I enjoy team work, taking direction well, while maintaining a voice on the project.<br>
+                I enjoy a challenge and welcome new skill sets.<br>
+                I am a jack of all trades, master of none, but better then a master of one.
+            </li>
+            <li>
+                <h2>Portfolio:</h2> This will be a gallery of the work I've done recently as well as example work to encourage others to step into web development.
+            </li>
+            <li>
+                <h2>Contact:</h2> Section to email me.<a href="https://www.linkedin.com/in/valente-meza-734202133">Linkedin,</a>
+                <a href="https://twitter.com/V_For_Valente">Twitter</a>
+            </li>
+        </ul>
+        <p>My wire frame was created using <a href="https://www.draw.io/" class="website">Draw.io</a></p>
+        <img src="images/wireframe.png">
+    </main>
     </body>
 </html>
