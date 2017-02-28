cae91345627f69e7d199cd00d054f71d5ebd25d7
Valente Meza <kingracoon64@gmail.com>
2017-02-27 22:16:14 -0700

    add twitter, github, and linkin  icons and links to nav-bar

--- public_html/index.php
+++ public_html/index.php
@@ -9,11 +9,14 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
     <!-- Optional theme -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
     <!-- My Custom CSS vmeza3@cnm.edu -->
-    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,12 +24,6 @@
 <!--    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
     <![endif]-->
-
-    <!-- jQuery (required for Bootstap's JS plugins) -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
     <title>Vale Por ABQ.</title>
 </head>
@@ -52,9 +49,27 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Portfolio</a></li>
-                        <li><a href="#">Kitty Treats</a></li>
+                        <li><a href="https://github.com/ValeMeza">
+                            <span class="fa-stack">
+                                <i class="fa fa-square-o fa-stack-2x"></i>
+                                <i class="fa fa-github-alt fa-stack-1x"> </i>
+                            </span>
+                            </a>
+                        </li>
+                        <li><a href="https://twitter.com/V_For_Valente">
+                                <span class="fa-stack">
+                                    <i class="fa fa-square-o fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li><a href="https://www.linkedin.com/in/valente-meza-734202133/">
+                                <span class="fa-stack">
+                                    <i class="fa fa-square-o fa-stack-2x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x"></i>
+                                </span>
+                            </a>
+                        </li>
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </nav>
@@ -94,13 +109,13 @@
 
             <!--begin 4 column row-->
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <div class="content-box">
                         <h3>Add link to GitHub</h3>
                         <p>I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come. Don't even trip about your pants, dawg. We got an extra pair right here. You're growing up fast, Morty. You're going into a great big thorn straight into my ass. I'm the Devil, what should I do when I fail? Give myself an ice cream? Aw, geez. Puh rum pum pow! Aw, c'mon Rick. That doesn't seem so bad. The fucking amish bitch shot me!</p>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-8">
                     <div class="content-box">
                         <h3>Swipebox</h3>
                         <p><a href="images/MyPersonalImage(3).JPG" class="swipebox" title="My Caption">
@@ -108,15 +123,10 @@
                             </a></p>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="content-box">
-                        <p></p>
-                    </div>
-                </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <div class="content-box">
                         <h3>Another Random Image</h3>
-                        <p><iframe src="//giphy.com/embed/HuXyLzDZpwfra?html5=true" width="270" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/HuXyLzDZpwfra">via GIPHY</a></p></p>
+                        <p><iframe src="//giphy.com/embed/HuXyLzDZpwfra?html5=true" width="150" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/HuXyLzDZpwfra">via GIPHY</a></p></p>
                     </div>
                 </div>
             </div>
