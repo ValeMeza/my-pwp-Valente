5cb76338ac0361515e914b723b236ab59ac1bb69
Valente Meza <kingracoon64@gmail.com>
2017-03-05 21:08:35 -0700

    main photo centered.

--- public_html/index.php
+++ public_html/index.php
@@ -55,18 +55,18 @@
             <nav class="navbar navbar-inverse">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+ <!--                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-                    </button>
+                    </button>-->
                     <a class="navbar-brand" >Vale Por ABQ.</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
+       <!--         <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">-->
                         <!--        <li><a href="https://github.com/ValeMeza">
                                         <span class="fa-stack">
                                             <i class="fa fa-square-o fa-stack-2x"></i>
@@ -90,107 +90,113 @@
                                 </li>
                             </ul>-->
                 </div><!-- /.navbar-collapse -->
-            </nav>
+         <!--   </nav>-->
         </div>
     </header>
 
     <!--begin main page content-->
     <main>
-        <div class="image-responsive">
-            <!--begin welcome row-->
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="grid">
-                        <figure class="effect-valente">
+        <!--begin welcome row-->
+   <!--     <div class="row">
+            <div class="col-md-8">-->
+                <div class="grid">
+                    <figure class="effect-valente">
+                        <div class="image-responsive">
                             <img src="images/rsz_img_0556.jpg" alt="rsz_img_0556" />
-                            <figcaption>
-                                <!-- TODO add more padding to h1 tags -->
-                                <h1>Vale <span>Valente</span></h1>
-                                <p>After becoming a Web Developer I seek to change lives with my skills. I want to teach, learn and build my way to helping Albuquerque become a bigger tech hub for the world.<br>
-                                    I am a Web Developer in the greater Albuquerque area.<br>
-                                    <a href="https://github.com/ValeMeza">
-                                <span class="fa-stack">
+                        </div>
+                        <figcaption>
+                            <!-- TODO add more padding to h1 tags -->
+                            <h1>Vale <span>Valente</span></h1>
+                            <p>After becoming a Web Developer I seek to change lives with my skills. I want to teach, learn and build my way to helping Albuquerque become a bigger tech hub for the world.<br>
+                                I am a Web Developer in the greater Albuquerque area.<br>
+                                <a href="https://github.com/ValeMeza">
+                                    <span class="fa fa-github-alt">
+                                  <!--  <span class="fa-stack">
                                     <i class="fa fa-square-o fa-stack-2x"></i>
-                                    <i class="fa fa-github-alt fa-stack-1x"> </i>
+                                    <i class="fa fa-github-alt fa-stack-1x"> </i>-->
                                 </span>
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/valente-meza-734202133/">
-                                <span class="fa-stack">
+                                </a>
+                                <a href="https://www.linkedin.com/in/valente-meza-734202133/">
+                                    <span class="fa fa-linkedin">
+                                <!--<span class="fa-stack">
                                     <i class="fa fa-square-o fa-stack-2x"></i>
-                                    <i class="fa fa-linkedin fa-stack-1x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x"></i>-->
                                 </span>
-                                    </a>
-                                    <a href="https://twitter.com/V_For_Valente">
-                                <span class="fa-stack">
+                                </a>
+                                <a href="https://twitter.com/V_For_Valente">
+                                    <span class="fa fa-twitter">
+                               <!-- <span class="fa-stack">
                                     <i class="fa fa-square-o fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x"></i>-->
                                 </span></p>
-                            </figcaption>
-                        </figure>
-                        <!-- ... -->
+                        </figcaption>
+                    </figure>
+                    <!-- ... -->
+                </div>
+  <!--          </div>
+        </div>
+        </div>-->
+    </main>
+<!--begin display column row-->
+
+<!--begin contact column row-->
+<h3>I always welcome projects.</h3>
+<div class="row">
+    <div class="col-md-8">
+        <div class="content-box">
+
+           Currently going to be attending Meet up events across Albuquerque. Feel free to contact me or just meet me.
+            <!-- list of events ill be attending for the month-->
+        </div>
+    </div>
+    <div class="col-md-4">
+        <h3>Let's get to work.</h3>
+        <form id="contact-form" action="php/mailer.php" method="post">
+            <div class="content-box">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <div class="col-m-10">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Name or Company" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <div class="col-m-10">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="subject">Subject</label>
+                    <div class="col-m-10">
+                        <div class="input-group">
+                            <input type="text" class="form control" id="subject" name="subject" placeholder="Subject">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <div class="col-m-10">
+                        <div class="input-group"></div>
+                        <textarea class="form-control" rows="4" name="message"></textarea>
                     </div>
                 </div>
             </div>
-            <!--begin display column row-->
-
-            <!--begin contact column row-->
-            <div class="row">
-                <div class="col-md-10">
-                    <div class="content-box">
-                        <h3></h3>
-                    I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come. Don't even trip about your pants, dawg. We got an extra pair right here. You're growing up fast, Morty. You're going into a great big thorn straight into my ass. I'm the Devil, what should I do when I fail? Give myself an ice cream? Aw, geez. Puh rum pum pow! Aw, c'mon Rick. That doesn't seem so bad. The fucking amish bitch shot me!
-                    </div>
+            <div class="g-recaptcha" data-sitekey="6LeqcRcUAAAAAOrSLzMxwLso7JRGZ9JSRDQjTsMP"></div>
+            <div class="form-group">
+                <div class="col-m-10 col-m-offset-2">
+                    <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
                 </div>
-                <div class="col-md-4">
-                    <h3>Let's get to work.</h3>
-                    <form id="contact-form" action="php/mailer.php" method="post">
-                        <div class="content-box">
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <div class="col-m-10">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Name or Company" value="">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <div class="col-m-10">
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="subject">Subject</label>
-                                <div class="col-m-10">
-                                    <div class="input-group">
-                                        <input type="text" class="form control" id="subject" name="subject" placeholder="Subject">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="message">Message</label>
-                                <div class="col-m-10">
-                                    <div class="input-group"></div>
-                                    <textarea class="form-control" rows="4" name="message"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="g-recaptcha" data-sitekey="6LeqcRcUAAAAAOrSLzMxwLso7JRGZ9JSRDQjTsMP"></div>
-                        <div class="form-group">
-                            <div class="col-m-10 col-m-offset-2">
-                                <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
-                            </div>
-                        </div>
-                    </form>
-                    <!--empty area for form error/success output-->
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div id="output-area"></div>
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </form>
+        <!--empty area for form error/success output-->
+        <div class="row">
+            <div class="col-xs-12">
+                <div id="output-area"></div>
             </div>
         </div>
-    </main>
-</div><!--.sfooter-content-->
+    </div>
+</div>
+<!--.sfooter-content-->
 
 <!--begin footer content-->
 <footer>
